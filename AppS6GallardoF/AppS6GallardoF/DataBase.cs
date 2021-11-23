7d1a0443c23d7c9591844baf3d5270dac3fc495c
Fernanda Gallardo <Fernanda Gallardo@DESKTOP-6UVI768>
2021-11-22 22:19:49 -0500

    Agregar archivos de proyecto.

---
+++ AppS6GallardoF/AppS6GallardoF/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppS6GallardoF
+{
+    public interface DataBase
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
---
+++ AppS6GallardoF/AppS6GallardoF/Registro.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppS6GallardoF.Models;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS6GallardoF
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        private SQLiteAsyncConnection con;
+        public Registro()
+        {
+            InitializeComponent();
+            con = DependencyService.Get<DataBase>().GetConnection();        }
+
+        private void bntAgregar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var Registros = new Estudiante { Nombre = txtNombre.Text, Usuario = txtUsuario.Text, Contrasenia = txtContrasenia.Text };
+                con.InsertAsync(Registros);
+                DisplayAlert("Alerta", "Dato ingresado", "ok");
+                txtNombre.Text = "";
+                txtContrasenia.Text = "";
+                txtUsuario.Text = "";
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "ok");
+            }
+        }
+    }
+}
